536ff5ec3a97c8afc54070c8787417417d688fe8
Enrique Jordano García <71704080+i92jogae@users.noreply.github.com>
2026-09-08 22:03:04 +0200

    Wrap app with Redux provider and strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import App from './App'
 import './index.css'
-import App from './App.tsx'
 import { store } from './store'
-import { Provider } from 'react-redux' 
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 )
