cf8a7d4aed3fb399a947d0704aa85ce3aacc8cd2
Enrique Jordano García <71704080+i92jogae@users.noreply.github.com>
2026-09-08 22:06:16 +0200

    Add React testing utilities

---
+++ src/test/test-utils.tsx
@@ -0,0 +1,29 @@
+import { render, type RenderOptions } from '@testing-library/react'
+import { configureStore } from '@reduxjs/toolkit'
+import type { ReactElement } from 'react'
+import { Provider } from 'react-redux'
+import { usersApi } from '../store/users/api'
+import usersUiReducer from '../store/users/slice'
+
+export const setupTestStore = () =>
+  configureStore({
+    reducer: {
+      usersUi: usersUiReducer,
+      [usersApi.reducerPath]: usersApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(usersApi.middleware)
+  })
+
+type TestStore = ReturnType<typeof setupTestStore>
+
+type ExtendedRenderOptions = Omit<RenderOptions, 'queries'> & {
+  store?: TestStore
+}
+
+export const renderWithProviders = (
+  ui: ReactElement,
+  { store = setupTestStore(), ...renderOptions }: ExtendedRenderOptions = {}
+) => ({
+  store,
+  ...render(<Provider store={store}>{ui}</Provider>, renderOptions)
+})
